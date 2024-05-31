b5a8a89f5f6e10cc62c24d29c93bf07bdefe2dca
ctcpip <ctcpip@users.noreply.github.com>
2024-05-31 17:01:04 -0500

    ⚡️ remove games after all players have quit, perf improvements

--- src/common.js
+++ src/common.js
@@ -1,1 +1,1 @@
-module.exports = { games: [] };
+module.exports = { games: {} };
--- src/message-handlers/connect.js
+++ src/message-handlers/connect.js
@@ -3,29 +3,22 @@ const { messageTypeEnum } = require('../message-type-enum');
 const { games } = require('../common');
 const handleError = require('../handle-error');
 
-module.exports = function(socket, rawData, message) {
-  let game = games.find(g => g.gameID === message.gameID);
+module.exports = function({ socket, message }) {
+  let game = games[message.gameID];
 
   if (!game) {
-    game = { gameID: message.gameID, players: [], seed: message.seed };
-    games.push(game);
+    game = { players: {}, seed: message.seed, heartbeat: Date.now() };
+    games[message.gameID] = game;
   }
 
   socket.send(JSON.stringify({ type: messageTypeEnum.SEED, seed: game.seed }), null, handleError);
 
-  for (let i = 0; i < game.players.length; i++) {
-    if (game.players[i].playerID === message.playerID) {
-    // remove existing player, this is a new connection
-      game.players.splice(i, 1);
-      break;
-    }
-  }
+  // replace existing player if they exist; this is a new connection
+  game.players[message.playerID] = new Player(message.playerID, socket);
 
-  game.players.push(new Player(message.playerID, socket));
+  const playerIDs = Object.keys(game.players);
 
-  const playerIDs = game.players.map(p => p.playerID);
-
-  for (const p of game.players) {
-    p.socket.send(JSON.stringify({ type: messageTypeEnum.CONNECT, players: playerIDs }), null, handleError);
+  for (const playerID of playerIDs) {
+    game.players[playerID].socket.send(JSON.stringify({ type: messageTypeEnum.CONNECT, players: playerIDs }), null, handleError);
   }
 };
